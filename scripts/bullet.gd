27f9be811ab9e8a762712719cba9533c10900a64
Niels <n.v.c.braam@gmail.com>
2017-04-11 22:57:58 +0200

    fixed the wind-coding, because it was wrong

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,6 +1,6 @@
 extends TestCube
 
-#Accuracy Internatiol AWM (Sniper Rifle)
+# Accuracy Internatiol AWM (Sniper Rifle)
 # Length = 1200 mm
 # Barrel Length = 660 mm
 # Cartridge = .300 Winchester Magnum
@@ -13,13 +13,15 @@ var grav_acc = Vector3(0,-9.81,0)
 var k = 0.000015
 # kg/m
 var number_x = range(-100,100)[randi()%range(-100,100).size()]
-var number_y = range(-100,100)[randi()%range(-100,100).size()]
+var number_z = range(-100,100)[randi()%range(-100,100).size()]
 
+onready var speed = 910 * get_translation().normalized()
 
 func _ready():
 	randomize()
+	
 	print(number_x)
-	print(number_y)
+	print(number_z)
 	#set_process(true)
 	#set_translation(bullet)
 	
@@ -27,25 +29,27 @@ func _ready():
 
 
 func _process(delta):
-	
 	var bullet = get_translation()
-	var speed = 910 * get_translation().normalized()
 	var force_gravity = mass * grav_acc
-	var wind = Vector3()
-	wind.x = number_x
-	wind.z = number_y
+	var wind_x = Vector3()
+	wind_x.x = number_x
+	var wind_z = Vector3()
+	wind_z.z = number_z
 	
 	
-	var force_wind = Vector3() * wind.normalized()
-	force_wind.x = k * wind.x * wind.x
-	force_wind.y = k * wind.y * wind.y
-	var acceleration = (force_wind + force_gravity)/mass
+	var force_wind_x = Vector3()
+	force_wind_x.x = k * wind_x.x * wind_x.x * wind_x.normalized().x
+	var force_wind_z = Vector3()
+	force_wind_z.z = k * wind_z.z * wind_z.z * wind_z.normalized().z
+	var acceleration = (force_wind_x + force_wind_z + force_gravity)/mass
+	
+	
 	speed += delta * acceleration
 	bullet = delta * speed + get_translation()
 	look_at(get_translation() + speed,Vector3(0,1,0))
 	set_translation(bullet)
-	#print(get_translation())
-	print(speed)
+	print(get_translation())
+	# print(speed)
 	
 	
 	pass
